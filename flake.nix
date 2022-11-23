c4b305f3e225edd05797e34bb6a0f95bb4b48208
Alfurqani <syifa.alfurqoni@gmail.com>
2022-11-23 16:44:20 +0700

    flake.nix

--- flake.nix
+++ flake.nix
@@ -1,28 +1,49 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  description = "Flake Configuration";
+  inputs = 
+  {
+    # nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-22.05";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager =
+    {
+    url = "github:nix-community/home-manager";
+    inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {self, home-manager, ...}@inputs:
     let
       system = "x86_64-linux";
-      # https://search.nixos.org/ <-- mencari packages apa saja yang
-      # tersedia di nix;
       pkgs = inputs.nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations = {
-        alfurqani = home-manager.lib.homeManagerConfiguration {
+
+      # pkgs = import nixpkgs
+      # {
+      #   inherit system;
+      #   config = 
+      #   { allowUnfree = true; };
+      # };
+
+      # lib = nixpkgs.lib;
+
+    in 
+    {
+      homeConfigurations = 
+      {
+        alfurqani = home-manager.lib.homeManagerConfiguration 
+        {
           inherit pkgs;
 
-          modules = [
-            ({pkgs, ...}:{
+          modules = 
+          [
+            ({pkgs, ...}:
+            {
               home.stateVersion = "22.05";
               home.username = "alfurqani";
               home.homeDirectory = "/home/alfurqani";
 
-              home.packages = with pkgs; [
+              home.packages = with pkgs; 
+              [
                 git
                 neofetch
               ];
@@ -30,6 +51,20 @@
           ];
         };
       };
+
+      # nixosConfigurations =
+      # {
+      #   nixos = lib.nixosSystem
+      #   {
+      #     inherit system;
+
+      #     modules = 
+      #     [
+      #       ./etc/nixos/configuration.nix
+      #     ];
+      #   };
+      # };
+
     };
   
 }
