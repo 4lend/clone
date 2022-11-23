2dad9979de1cebe286b2efc558b058366f925f40
Alfurqani <syifa.alfurqoni@gmail.com>
2022-11-23 11:19:41 +0700

    basic

--- flake.nix
+++ flake.nix
@@ -7,20 +7,20 @@
 
   outputs = {self, home-manager, ...}@inputs:
     let
-      system = "aarch64-darwin";
+      system = "x86_64-linux";
       # https://search.nixos.org/ <-- mencari packages apa saja yang
       # tersedia di nix;
       pkgs = inputs.nixpkgs.legacyPackages.${system};
     in {
       homeConfigurations = {
-        rin = home-manager.lib.homeManagerConfiguration {
+        alfurqani = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
 
           modules = [
             ({pkgs, ...}:{
               home.stateVersion = "22.05";
-              home.username = "rin";
-              home.homeDirectory = "/Users/rin";
+              home.username = "alfurqani";
+              home.homeDirectory = "/home/alfurqani";
 
               home.packages = with pkgs; [
                 git
